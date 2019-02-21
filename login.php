c24e3afec18472baf5c0efe9f7908194efd9955c
Abdul Rahman Wahid <abrahmanw@gmail.com>
2019-02-21 18:00:50 +0700

    mod login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-	<div id="logo" align="center"><img src="<?php echo base_url();?>/images/logo50.png"></div>
+	<div id="logo" align="center"><img src="<?php echo base_url();?>/images/logo.png"></div>
 
 	<div id="login">
 		<?php echo form_open('login') ?>
